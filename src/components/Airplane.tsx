d43b5895ba3095fdce9d27b98ff71fb8bc8bd3eb
Florin Chelaru <florin-chelaru@users.noreply.github.com>
2023-02-06 20:55:01 +0200

    Add extra details to airplane

--- src/components/Airplane.tsx
+++ src/components/Airplane.tsx
@@ -41,30 +41,99 @@ const Airplane = React.forwardRef<THREE.Mesh, ThreeElements['mesh']>((props, ref
         <meshPhongMaterial color={Colors.red} flatShading={true} />
       </mesh>
       {/*  Engine  */}
-      <mesh position={[40, 0, 0]} castShadow={true} receiveShadow={true}>
+      <mesh position={[50, 0, 0]} castShadow={true} receiveShadow={true}>
         <boxGeometry args={[20, 50, 50, 1, 1, 1]} />
         <meshPhongMaterial color={Colors.white} flatShading={true} />
       </mesh>
       {/*  Tail  */}
-      <mesh position={[-35, 25, 0]} castShadow={true} receiveShadow={true}>
+      <mesh position={[-40, 20, 0]} castShadow={true} receiveShadow={true}>
         <boxGeometry args={[15, 20, 5, 1, 1, 1]} />
         <meshPhongMaterial color={Colors.red} flatShading={true} />
       </mesh>
       {/*  Wing  */}
-      <mesh castShadow={true} receiveShadow={true}>
-        <boxGeometry args={[40, 8, 150, 1, 1, 1]} />
+      <mesh position={[0, 15, 0]} castShadow={true} receiveShadow={true}>
+        <boxGeometry args={[30, 5, 120, 1, 1, 1]} />
         <meshPhongMaterial color={Colors.red} flatShading={true} />
       </mesh>
+      {/*  Windshield  */}
+      <mesh position={[5, 27, 0]} castShadow={true} receiveShadow={true}>
+        <boxGeometry args={[3, 15, 20, 1, 1, 1]} />
+        <meshPhongMaterial
+          color={Colors.white}
+          flatShading={true}
+          transparent={true}
+          opacity={0.3}
+        />
+      </mesh>
       {/*  Propeller  */}
-      <mesh ref={propellerRef} position={[50, 0, 0]} castShadow={true} receiveShadow={true}>
+      <mesh ref={propellerRef} position={[60, 0, 0]} castShadow={true} receiveShadow={true}>
         <boxGeometry args={[20, 10, 10, 1, 1, 1]} />
         <meshPhongMaterial color={Colors.brown} flatShading={true} />
-        {/*  Blade  */}
+        {/*  Blade 1 */}
         <mesh position={[8, 0, 0]} castShadow={true} receiveShadow={true}>
-          <boxGeometry args={[1, 100, 20, 1, 1, 1]} />
+          <boxGeometry args={[1, 80, 10, 1, 1, 1]} />
+          <meshPhongMaterial color={Colors.brownDark} flatShading={true} />
+        </mesh>
+        {/*  Blade 2 */}
+        <mesh
+          position={[8, 0, 0]}
+          castShadow={true}
+          receiveShadow={true}
+          rotation={[Math.PI / 2, 0, 0]}>
+          <boxGeometry args={[1, 80, 10, 1, 1, 1]} />
           <meshPhongMaterial color={Colors.brownDark} flatShading={true} />
         </mesh>
       </mesh>
+      {/* Wheel Protection Right */}
+      <mesh position={[25, -20, 25]} castShadow={true} receiveShadow={true}>
+        <boxGeometry args={[30, 15, 10]} />
+        <meshPhongMaterial color={Colors.red} flatShading={true} />
+      </mesh>
+      {/* Wheel Protection Left */}
+      <mesh position={[25, -20, -25]} castShadow={true} receiveShadow={true}>
+        <boxGeometry args={[30, 15, 10]} />
+        <meshPhongMaterial color={Colors.red} flatShading={true} />
+      </mesh>
+
+      {/* Wheel Right */}
+      <mesh position={[25, -28, 25]} castShadow={true} receiveShadow={true}>
+        <boxGeometry args={[24, 24, 4]} />
+        <meshPhongMaterial color={Colors.brownDark} flatShading={true} />
+        {/* Axis */}
+        <mesh castShadow={true} receiveShadow={true}>
+          <boxGeometry args={[10, 10, 6]} />
+          <meshPhongMaterial color={Colors.brown} flatShading={true} />
+        </mesh>
+      </mesh>
+      {/* Wheel Left */}
+      <mesh position={[25, -28, -25]} castShadow={true} receiveShadow={true}>
+        <boxGeometry args={[24, 24, 4]} />
+        <meshPhongMaterial color={Colors.brownDark} flatShading={true} />
+        {/* Axis */}
+        <mesh castShadow={true} receiveShadow={true}>
+          <boxGeometry args={[10, 10, 6]} />
+          <meshPhongMaterial color={Colors.brown} flatShading={true} />
+        </mesh>
+      </mesh>
+      {/* Wheel Back */}
+      <mesh position={[-35, -5, 0]} scale={[0.5, 0.5, 0.5]} castShadow={true} receiveShadow={true}>
+        <boxGeometry args={[24, 24, 4]} />
+        <meshPhongMaterial color={Colors.brownDark} flatShading={true} />
+        {/* Axis */}
+        <mesh castShadow={true} receiveShadow={true}>
+          <boxGeometry args={[10, 10, 6]} />
+          <meshPhongMaterial color={Colors.brown} flatShading={true} />
+        </mesh>
+      </mesh>
+
+      {/* Suspension */}
+      <mesh position={[-35, -5, 0]} rotation-z={-0.3} castShadow={true} receiveShadow={true}>
+        <mesh position-y={10}>
+          <boxGeometry args={[4, 20, 4]} />
+          <meshPhongMaterial color={Colors.red} flatShading={true} />
+        </mesh>
+      </mesh>
+
       <Pilot position={[-10, 27, 0]} />
     </mesh>
   )
--- src/util/geometryUtil.ts
+++ src/util/geometryUtil.ts
@@ -4,8 +4,6 @@ export function getVertices(geometry: THREE.BufferGeometry): THREE.Vector3[] {
   // Idea from: https://stackoverflow.com/questions/69720682/vertices-does-not-exist-on-type-buffergeometry-in-threejs
   const position = geometry.getAttribute('position') as THREE.BufferAttribute
 
-  console.log(`before: ${JSON.stringify(position.array)}`)
-
   return Array.from({ length: position.count }, (_, i) => {
     const v = new THREE.Vector3().fromBufferAttribute(position, i)
     console.log(v.toArray())
@@ -16,11 +14,66 @@ export function getVertices(geometry: THREE.BufferGeometry): THREE.Vector3[] {
 export function updateVertices(geometry: THREE.BufferGeometry, vertices: THREE.Vector3[]) {
   // Idea from: https://stackoverflow.com/questions/69720682/vertices-does-not-exist-on-type-buffergeometry-in-threejs
   const position = geometry.getAttribute('position') as THREE.BufferAttribute
-  vertices.forEach((v, i) => {
-    console.log(v.toArray())
-    position.setXYZ(i, v.x, v.y, v.z)
-  })
-  console.log(`after: ${JSON.stringify(position.array)}`)
-  console.log(position.array.length)
+  vertices.forEach((v, i) => position.setXYZ(i, v.x, v.y, v.z))
   position.needsUpdate = true
+}
+
+const BOX_EQUIVALENT_INDICES = new Map<number, number[]>([
+  [0, [0, 11, 17]],
+  [11, [0, 11, 17]],
+  [17, [0, 11, 17]],
+
+  [1, [1, 9, 20]],
+  [9, [1, 9, 20]],
+  [20, [1, 9, 20]],
+
+  [2, [2, 13, 19]],
+  [13, [2, 13, 19]],
+  [19, [2, 13, 19]],
+
+  [3, [3, 15, 22]],
+  [15, [3, 15, 22]],
+  [22, [3, 15, 22]],
+
+  [4, [4, 8, 21]],
+  [8, [4, 8, 21]],
+  [21, [4, 8, 21]],
+
+  [5, [5, 10, 16]],
+  [10, [5, 10, 16]],
+  [16, [5, 10, 16]],
+
+  [6, [6, 14, 23]],
+  [14, [6, 14, 23]],
+  [23, [6, 14, 23]],
+
+  [7, [7, 12, 18]],
+  [12, [7, 12, 18]],
+  [18, [7, 12, 18]]
+])
+
+/**
+ * Offsets the value of the vertex corresponding to the given index by the given coordinates.
+ * @param geometry
+ * @param i
+ * @param offset
+ */
+export function offsetVertex(
+  geometry: THREE.BoxGeometry,
+  i: number,
+  offset: THREE.Vector3 | [x: number, y: number, z: number]
+) {
+  const position = geometry.getAttribute('position') as THREE.BufferAttribute
+  if (Array.isArray(offset)) {
+    offset = new THREE.Vector3(offset[0], offset[1], offset[2])
+  }
+
+  const equivalentIndices = BOX_EQUIVALENT_INDICES.get(i)
+  if (equivalentIndices) {
+    const x = position.getX(i) + offset.x
+    const y = position.getY(i) + offset.y
+    const z = position.getZ(i) + offset.z
+    equivalentIndices.forEach((index) => position.setXYZ(index, x, y, z))
+    position.needsUpdate = true
+  }
 }
